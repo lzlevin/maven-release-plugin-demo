2664dacc0763a1480567652c9973a05fa48e0bab
Levin <lzlevin@qq.com>
2020-09-20 15:20:22 +0800

    dev:add test main method

---
+++ module-test/src/main/java/com/vin/Test.java
@@ -0,0 +1,13 @@
+package com.vin;
+
+/**
+ * <br>
+ *
+ * @author levin
+ * @since 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("dev add main method");
+    }
+}
